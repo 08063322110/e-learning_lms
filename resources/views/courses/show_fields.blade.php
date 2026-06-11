5ba49c24225c612f723079509ff86fc321cfa250
Ettah Michael Adeiza <mikettahadson@yahoo.com>
2026-06-11 22:17:00 +0100

    Fix course show and table view issues

--- resources/views/courses/show_fields.blade.php
+++ resources/views/courses/show_fields.blade.php
@@ -1,27 +1,70 @@
+<!-- Title Field -->
+<div class="form-group col-xs-12">
+    <h2>{!! $course->title !!}</h2>  
+    <p>{{ $course->sub_title }}
+        <div class="text-muted">
+            @if ($course->subscriber_count>0)
+            | students : {{ number_format($course->subscriber_count) }}
+            @endif   
+            @if ($course->subscriber_count>0)
+            | views : {{ number_format($course->view_count) }}
+            @endif 
+        </div>
+    </p>
+
+</div>
+
+@if (Auth::user()->role_id < 3  || Auth::user()->id == $course->user_id)
+    
+
+<!-- Creator Status Field -->
+<div class="col-md-5">
+    {!! Form::label('creator_status', 'Creator Status:') !!}
+    <p>
+        @if($course->creator_status == 1)
+             on
+        @else
+             off
+        @endif
+    </p>
+</div>
+
+<!-- Admin Status Field -->
+<div class="col-sm-7">
+    {!! Form::label('admin_status', 'Admin Status:') !!}
+    <p>
+           @if($course->admin_status == 1)
+             on
+        @else
+             off
+        @endif
+    </p>
+</div>
+@endif
+
 <!-- User Id Field -->
-<div class="col-sm-12">
-    {!! Form::label('user_id', 'User Id:') !!}
-    <p>{{ $course->user_id }}</p>
+<div class="col-md-5">
+    {!! Form::label('user_id', 'Author:') !!}
+    <p>{{ $course->user['name'] }}</p>
 </div>
 
 <!-- Category Id Field -->
-<div class="col-sm-12">
-    {!! Form::label('category_id', 'Category Id:') !!}
-    <p>{{ $course->category_id }}</p>
+<div class="col-md-5">
+    {!! Form::label('category_id', 'Category:') !!}
+    <p><a href="/categories/{!! $course->category['id'] !!}">{{ $course->category['name']}}</a></p>
 </div>
 
-<!-- Title Field -->
-<div class="col-sm-12">
-    {!! Form::label('title', 'Title:') !!}
-    <p>{{ $course->title }}</p>
+{{-- Updated at Field --}}
+<div class="col-md-5">
+    {!! Form::label('updated_at', 'Last Updated:') !!}
+    <p>{{ $course->updated_at }}</p>
 </div>
 
-<!-- Sub Title Field -->
-<div class="col-sm-12">
-    {!! Form::label('sub_title', 'Sub Title:') !!}
-    <p>{{ $course->sub_title }}</p>
+{{-- Created at Field --}}
+<div class="col-md-5">
+    {!! Form::label('created_at', 'Created At:') !!}
+    <p>{{ $course->created_at }}</p>
 </div>
-
 <!-- Description Field -->
 <div class="col-sm-12">
     {!! Form::label('description', 'Description:') !!}
@@ -29,61 +72,50 @@
 </div>
 
 <!-- About Instructor Field -->
-<div class="col-sm-12">
+<div class="col-md-8">
     {!! Form::label('about_instructor', 'About Instructor:') !!}
     <p>{{ $course->about_instructor }}</p>
 </div>
 
-<!-- Playlist Url Field -->
-<div class="col-sm-12">
-    {!! Form::label('playlist_url', 'Playlist Url:') !!}
-    <p>{{ $course->playlist_url }}</p>
-</div>
+
 
 <!-- Tags Field -->
-<div class="col-sm-12">
+<div class="col-md-8">
     {!! Form::label('tags', 'Tags:') !!}
     <p>{{ $course->tags }}</p>
 </div>
 
-<!-- Photo Field -->
-<div class="col-sm-12">
-    {!! Form::label('photo', 'Photo:') !!}
-    <p>{{ $course->photo }}</p>
-</div>
 
-<!-- Promo-Video Url Field -->
-<div class="col-sm-12">
-    {!! Form::label('promo-video_url', 'Promo-Video Url:') !!}
-    <p>{{ $course->promo-video_url }}</p>
-</div>
 
-<!-- Creator Status Field -->
-<div class="col-sm-12">
-    {!! Form::label('creator_status', 'Creator Status:') !!}
-    <p>{{ $course->creator_status }}</p>
-</div>
+<!-- Promo Video Url Field -->
+{{-- <div class="col-sm-12">
+    {!! Form::label('promo_video_url', 'Promo Video Url:') !!}
+    <p>{{ $course->promo_video_url }}</p>
+</div> --}}
 
-<!-- Admin Status Field -->
-<div class="col-sm-12">
-    {!! Form::label('admin_status', 'Admin Status:') !!}
-    <p>{{ $course->admin_status }}</p>
-</div>
+<!-- Playlist Url Field -->
+{{-- <div class="col-sm-12">
+    {!! Form::label('playlist_url', 'Playlist Url:') !!}
+    <p>{{ $course->playlist_url }}</p>
+</div> --}}
+
+
 
 <!-- What Will Students Learn Field -->
-<div class="col-sm-12">
+<div class="col-md-8">
     {!! Form::label('what_will_students_learn', 'What Will Students Learn:') !!}
     <p>{{ $course->what_will_students_learn }}</p>
 </div>
 
-<!-- Target Students Field -->
-<div class="col-sm-12">
+ <!-- Target Students Field -->
+
+<div class="col-md-8">
     {!! Form::label('target_students', 'Target Students:') !!}
     <p>{{ $course->target_students }}</p>
 </div>
 
 <!-- Requirements Field -->
-<div class="col-sm-12">
+<div class="col-md-8">
     {!! Form::label('requirements', 'Requirements:') !!}
     <p>{{ $course->requirements }}</p>
 </div>
@@ -100,15 +132,4 @@
     <p>{{ $course->actual_price }}</p>
 </div>
 
-<!-- View Count Field -->
-<div class="col-sm-12">
-    {!! Form::label('view_count', 'View Count:') !!}
-    <p>{{ $course->view_count }}</p>
-</div>
-
-<!-- Subscriber Count Field -->
-<div class="col-sm-12">
-    {!! Form::label('subscriber_count', 'Subscriber Count:') !!}
-    <p>{{ $course->subscriber_count }}</p>
-</div>
 
--- resources/views/courses/table.blade.php
+++ resources/views/courses/table.blade.php
@@ -1,42 +1,88 @@
 <div class="table-responsive">
     <table class="table" id="courses-table">
-        <thead>
-        <tr>
-           
-        <th>Title</th>
-        <th>Discount Price</th>
-        <th>Actual Price</th>
-            <th colspan="3">Action</th>
-        </tr>
+        <thead >
+            <tr >
+                {{-- <th>Photo</th> --}}
+                {{-- <th>Courses</th> --}}
+                {{-- <th>Price</th> --}}
+                <th class="text-right" colspan="120">Action</th>
+            </tr>
         </thead>
+
         <tbody>
+
         @foreach($courses as $course)
             <tr>
-            <td>{{ $course->photo }}</td>
-            <td><h2>{{ $course->title }}</h2></br> 
-               {{ $course->sub_title }} 
+                <td>{{ $course->photo }}</td>
+
+                <td width="80%">
+                    <h4 style="margin-bottom: -10px;">
+                        <a href="{{ route('courses.show', [$course->id]) }}">
+                            {{ $course->title }}
+                        </a>
+                    </h4>
+
+                    <br>
+               <div class="text-muted"> Author: {{ $course->user['name'] }} 
+            @if ($course->subscriber_count>0)
+            | students : {{ number_format($course->subscriber_count) }}
+            @endif   
+            @if ($course->subscriber_count>0)
+            | views : {{ number_format($course->view_count) }}
+            @endif 
+            </div>
+
+                
+                {!!$course->sub_title!!}
+                    
                 </td>
-            <td>{{ $course->promo_video_url }}</td>
-            <td>{{ $course->discount_price }}</td>
-            <td>{{ $course->actual_price }}</td>
-           
-            <td width="120">
-                    {!! Form::open(['route' => ['courses.destroy', $course->id], 'method' => 'delete']) !!}
-                    <div class='btn-group'>
+
+                <td>
+                    <h3 style="margin-bottom: -10px;">
+                        ${{ $course->discount_price }}
+                    </h3>
+
+                    <del style="text-decoration: line-through; padding-left: 10px">
+                        ${{ $course->actual_price }}
+                    </del>
+                </td>
+
+                <td width="120">
+                 @if(Auth::user()->role_id < 3 || $course->user_id == Auth::user()->id)
+
+                    {!! Form::open([
+                        'route' => ['courses.destroy', $course->id],
+                        'method' => 'delete'
+                    ]) !!}
+
+                    <div class="btn-group">
                         <a href="{{ route('courses.show', [$course->id]) }}"
-                           class='btn btn-default btn-xs'>
+                           class="btn btn-default btn-xs">
                             <i class="far fa-eye"></i>
                         </a>
+
                         <a href="{{ route('courses.edit', [$course->id]) }}"
-                           class='btn btn-default btn-xs'>
+                           class="btn btn-default btn-xs">
                             <i class="far fa-edit"></i>
                         </a>
-                        {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+
+                        {!! Form::button(
+                            '<i class="far fa-trash-alt"></i>',
+                            [
+                                'type' => 'submit',
+                                'class' => 'btn btn-danger btn-xs',
+                                'onclick' => "return confirm('Are you sure?')"
+                            ]
+                        ) !!}
                     </div>
+
                     {!! Form::close() !!}
+                    @endif
                 </td>
             </tr>
+
         @endforeach
+
         </tbody>
     </table>
 </div>
