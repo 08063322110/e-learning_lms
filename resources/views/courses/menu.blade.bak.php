fcc53b693580a2c21e33b97f15cb9b134faa155f
Ettah Michael Adeiza <mikettahadson@yahoo.com>
2026-07-28 19:08:08 +0100

    chore: remove backup menu file

---
+++ resources/views/courses/menu.blade.bak.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+<div class="col-md-12">
+<ul class="nav nav-pills text-bold">
+  <li role="presentation" ><a href="{{ route('courses.show', ['course' => $course->id]) }}">Course Home</a></li>
+  <li role="presentation"><a href="{{ route('courses.contents', ['course_id' => $course->id])}}">Contents</a></li>
+  {{-- <li role="presentation"><a href="#">Comments and Reviews</a></li> --}}
+  {{-- <li role="presentation"><a href="{{ route('courses.contents', ['course_id' => $course->id])}}">Contents</a></li> --}}
+
+
+  @if (Auth::check() AND (Auth::user()->id == $course->user_id || Auth::user()->role_id < 3 )) 
+    <li role="presentation"><a href="{{route('courses.subscribers', ['course_id' => $course->id])}}">Subscribers</a></li>
+  @endif
+  
+</ul>
+</div>
