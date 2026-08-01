803d085aa5e42b49334e2e6586fcee510c4420fb
Ettah Michael Adeiza <mikettahadson@yahoo.com>
2026-08-01 12:18:02 +0100

    Update navigation menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,12 +1,7 @@
 <form class="form-inline ml-3">
     <div class="input-group input-group-sm">
-
-        <input class="form-control border-left-0"
-               type="search"
-               placeholder="Search"
-               aria-label="Search">
-
-                <div class="input-group-prepend">
+        <input class="form-control border-left-0" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-prepend">
             <span class="input-group-text bg-white border-right-0">
                 <i class="fas fa-search"></i>
             </span>
@@ -14,6 +9,28 @@
     </div>
 </form>
 
+{{-- EVERYONE: Students, Teacher, Admin --}}
+<li class="nav-item">
+    <a href="{{ route('courses.index') }}"
+       class="nav-link {{ Request::is('courses*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-book"></i>
+        <p>Browse Courses</p>
+    </a>
+</li>
+
+{{-- STUDENT ONLY --}}
+@if(Auth::user()->role_id == 4)
+<li class="nav-item">
+<a href="{{ route('courses.index', ['my' => 1]) }}"
+           class="nav-link {{ Request::is('my-courses*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-graduation-cap"></i>
+        <p>My Courses</p>
+    </a>
+</li>
+@endif
+
+{{-- TEACHER + ADMIN ONLY --}}
+@if(Auth::user()->role_id < 4)
 <li class="nav-item">
     <a href="{{ route('categories.index') }}"
        class="nav-link {{ Request::is('categories*') ? 'active' : '' }}">
@@ -21,8 +38,10 @@
         <p>Course Categories</p>
     </a>
 </li>
+@endif
 
-
+{{-- ADMIN ONLY --}}
+@if(Auth::user()->role_id < 3)
 <li class="nav-item">
     <a href="{{ route('courseUsers.index') }}"
        class="nav-link {{ Request::is('courseUsers*') ? 'active' : '' }}">
@@ -31,45 +50,19 @@
     </a>
 </li>
 
-{{-- Moderator --}}
-@if (Auth::user()->role_id < 4)
-{{--     
 <li class="nav-item">
-    <a href="{{ route('courses.index') }}"
-       class="nav-link {{ Request::is('courses*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-book"></i>
-        <p>Course Categories</p>
-    </a>
-</li> --}}
-
-@endif
-
-{{-- Admin Section --}}
-
-@if(Auth::user()->role_id < 3)
-
-<li class="nav-item">
-    <a href="{{ route('courses.index') }}"
-       class="nav-link {{ Request::is('courses*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-book"></i>
-        <p>Courses</p>
+    <a href="{{ route('coupons.index') }}"
+       class="nav-link {{ Request::is('coupons*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-ticket-alt"></i>
+        <p>Coupons</p>
     </a>
 </li>
-
 
 <li class="nav-item">
     <a href="{{ route('comments.index') }}"
        class="nav-link {{ Request::is('comments*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-comments"></i>
         <p>Comments</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{ route('items.index') }}"
-       class="nav-link {{ Request::is('items*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-list"></i>
-        <p>Items</p>
     </a>
 </li>
 
@@ -90,26 +83,10 @@
 </li>
 
 <li class="nav-item">
-    <a href="{{ route('views.index') }}"
-       class="nav-link {{ Request::is('views*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-eye"></i>
-        <p>Views</p>
-    </a>
-</li>
-
-<li class="nav-item">
     <a href="{{ route('roles.index') }}"
        class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-user-shield"></i>
         <p>Roles</p>
     </a>
 </li>
-
-@endif<li class="nav-item">
-    <a href="{{ route('coupons.index') }}"
-       class="nav-link {{ Request::is('coupons*') ? 'active' : '' }}">
-        <p>Coupons</p>
-    </a>
-</li>
-
-
+@endif
